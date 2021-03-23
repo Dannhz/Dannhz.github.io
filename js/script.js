5d96e5a539ddad096c6bf948ce96ac2fdb3e7e83
Dannhz <danilocaetanobarreto@gmail.com>
2021-03-23 20:23:37 -0300

    Otimização e adição de componentes

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const layer_2 = document.getElementById('layer2');
 const layer_3 = document.getElementById('layer3');
 const layer_4 = document.getElementById('layer4');
 const layer_5 = document.getElementById('layer5');
+var waiting = false;
+
 
 const layerMoon = document.getElementById('moon');
 const layerMoonLight = document.getElementById('moonLight');
@@ -31,32 +33,56 @@ btnsModalClose.forEach(function(btn){
 window.onclick = function(e){
     if(e.target.className === "modal"){
         e.target.style.display = "none";
+        
         document.body.style.overflow = "scroll";
     }
 }
 
-//Listeners
-window.addEventListener("scroll", animeScroll, false);
+window.onscroll = (function(){
+    if(waiting){
+        return;
+    }
+    waiting = true;
+    console.log("scrollo");
+    animeScroll();
 
+    setTimeout(function(){
+        waiting = false
+    }, 25);
+});
 
 //Funções
-function animeScroll(){
-    layer_1.style.top = -(window.pageYOffset/1.4) + 'px';
-    layer_2.style.top = -(window.pageYOffset/2) + 'px';
-    layer_3.style.top = -(window.pageYOffset/5) + 'px';
-    layer_4.style.top = -(window.pageYOffset/6) + 'px';
-    layer_5.style.top = -(window.pageYOffset/9) + 'px';
+var animeScroll = function(){
 
-    var bodyRect = document.body.getBoundingClientRect();
-    var moonRect = layerMoon.getBoundingClientRect();
+    //Efeito Parallax - Header
+        //Evita que o efeito continue quando o header não estiver visível
+    if(window.pageYOffset <= 900){ 
+        layer_1.style.top = -(window.pageYOffset/1.4) + 'px';
+        layer_2.style.top = -(window.pageYOffset/2) + 'px';
+        layer_3.style.top = -(window.pageYOffset/5) + 'px';
+        layer_4.style.top = -(window.pageYOffset/6) + 'px';
+        layer_5.style.top = -(window.pageYOffset/9) + 'px';
+    }
+
+    //Efeito Parallax - Lua
     var moonLightRect = layerMoonLight.getBoundingClientRect();
+    moonLightRectInicio = moonLightRect.top - window.innerHeight + 150;
 
-    layerMoonLight.style.opacity = 1 - ((moonLightRect.top -70)/300)
-    layerMoon.style.backgroundPositionY = (moonRect.top  /.66)  + 'px';
 
-    const windowTop = window.pageYOffset + ((window.innerHeight *9)/11);
-  
+        //Garante que o efeito só aconteça quando a imagem estiver visível
+    if(moonLightRectInicio <= 0 && moonLightRectInicio >= -1000){
+        opacidadeLua = layerMoonLight.style.opacity;
+        formulaOpacidade = (-(moonLightRectInicio/500)).toFixed(1);
+        if(formulaOpacidade > 0 && formulaOpacidade < 1 && opacidadeLua != formulaOpacidade){
+            layerMoonLight.style.opacity = formulaOpacidade;
+        }
+        var moonRect = layerMoon.getBoundingClientRect();
+        layerMoon.style.backgroundPositionY = (moonRect.top/.66)  + 'px';
+    }
 
+    //Animação de elementos
+    const windowTop = window.pageYOffset + (window.innerHeight * .8);
+    var bodyRect = document.body.getBoundingClientRect();
     target.forEach(function(element){
         var elemRect = element.getBoundingClientRect();
         var offset = elemRect.top - bodyRect.top;
